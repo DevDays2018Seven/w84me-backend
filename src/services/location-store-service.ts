9bef8f34b24bfc2f013fb3dd7e968da4b2dc0ccb
Hendrik Pilz <hepisec@gmail.com>
2018-04-09 12:11:00 +0200

    Read locations from locations.json

--- src/services/location-store-service.ts
+++ src/services/location-store-service.ts
@@ -1,6 +1,7 @@
 import { injectable } from "inversify";
 
 import { Location } from "../models/location";
+import { readFileSync } from "fs";
 
 @injectable()
 export class LocationStore {
@@ -8,7 +9,27 @@ export class LocationStore {
     private readonly locations: Array<Location>;
 
     public constructor() {
-        this.locations = [];
+        this.locations = this.readLocationsFromStaticFile("locations.json");
+        this.locations.sort(this.sortLocationsByName);
+    }
+
+    public readLocationsFromStaticFile(path: string): Array<Location> {
+        const locations = JSON.parse(readFileSync(path).toString());
+        const locationArray = new Array<Location>();
+
+        for (let location of locations) {
+            locationArray.push(Location.fromJson(location));
+        }
+
+        return locationArray;
+    }
+
+    private sortLocationsByName(a: Location, b: Location): number {
+        if (a.name === b.name) {
+            return 0;
+        }
+
+        return a.name > b.name ? 1 : -1;
     }
 
     public addLocation(location: Location): void {
