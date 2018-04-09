150492416a8112ca7c085219f434a8ad8db06666
Hendrik Pilz <hepisec@gmail.com>
2018-04-09 15:32:38 +0200

    Create session seed data

--- src/controllers/estimates-controller.ts
+++ src/controllers/estimates-controller.ts
@@ -4,10 +4,13 @@ import { inject } from "inversify";
 
 import TYPES from "../constant/types";
 import { EstimatesService } from "../services/estimates-service";
+import { LocationStore } from "../services/location-store-service";
+import { isNullOrUndefined } from "util";
 
 @controller("/estimates", TYPES.LoggerMiddleware)
 export class EstimatesController {
 
+    @inject(TYPES.LocationStore) private locationStore: LocationStore;
     @inject(TYPES.EstimatesService) private estimatesService: EstimatesService;
 
     @httpGet("/")
@@ -15,6 +18,11 @@ export class EstimatesController {
         @requestBody("locationId") locationId: number,
         @response() res: e.Response
     ): void {
+        if (isNullOrUndefined(this.locationStore.findLocation(locationId))) {
+            res.status(400).end();
+            return;
+        }
+
         res.status(200).json(this.estimatesService.calculateEstimates(locationId));
     }
 
--- src/services/session-store-service.ts
+++ src/services/session-store-service.ts
@@ -1,14 +1,49 @@
-import { injectable } from "inversify";
+import { injectable, inject } from "inversify";
 
 import { Session } from "../models/session";
+import TYPES from "../constant/types";
+import { IdGenerator } from "../services/id-generator-service";
 
 @injectable()
 export class SessionStore {
 
+    @inject(TYPES.IdGenerator) private idGenerator: IdGenerator;
     private readonly sessions: Array<Session>;
 
     public constructor() {
         this.sessions = [];
+    }
+
+    public seedSessions(): void {
+        let generatedSessionId = this.idGenerator.generateSessionId();
+
+        // seed finished Sessions
+        for (let i = 1; i < 50; i++) {
+            const locationId = Math.floor(Math.random() * 10) + 1;
+            const sessionStart = this.randomDate(new Date(2018, 2, 1), new Date(2018, 3, 1)).getTime();
+            const sessionEnd = sessionStart + Math.floor(Math.random() * 60);
+
+            this.addSession(new Session(generatedSessionId, locationId, sessionStart));
+            this.addSession(new Session(generatedSessionId, locationId, sessionEnd));
+            generatedSessionId = this.idGenerator.generateSessionId();
+        }
+
+        // seed open Sessions
+        const today = new Date();
+
+        for (let i = 1; i < 50; i++) {
+            const locationId = Math.floor(Math.random() * 10) + 1;
+            const sessionStart = this.randomDate(today, today).getTime() - + Math.floor(Math.random() * 60);
+
+            this.addSession(new Session(generatedSessionId, locationId, sessionStart));
+            generatedSessionId = this.idGenerator.generateSessionId();
+        }        
+
+        console.log(this.sessions);
+    }
+
+    private randomDate(start, end) {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
     }
 
     public addSession(session: Session): void {
