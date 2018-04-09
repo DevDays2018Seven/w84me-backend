7979aa9359ada0b5d701e80ee31ea68294f440d0
Andreas Pfohl <mail@apfohl.com>
2018-04-09 12:25:37 +0200

    Added getter and setter for Session model

--- src/models/session.ts
+++ src/models/session.ts
@@ -7,24 +7,54 @@ export class Session {
         private _timestampEnd: number
     ) {}
 
+    get id(): number {
+        return this._id;
+    }
 
+    set id(value: number) {
+        this._id = value;
+    }
 
-    // public toJson(): any {
-    //     return {
-    //         id: this.id,
-    //         locationId: this.locationId,
-    //         timestampStart: this.timestampStart,
-    //         timeStampEnd: this.timestampEnd
-    //     }
-    // }
+    get locationId(): number {
+        return this._locationId;
+    }
 
-    // public static fromJson(json: any): Session {
-    //     return new Location(
-    //         json.id,
-    //         json.locationId,
-    //         json.timestampStart,
-    //         json.timestampEnd
-    //     );
-    // }
+    set locationId(value: number) {
+        this._locationId = value;
+    }
+
+    get timestampStart(): number {
+        return this._timestampStart;
+    }
+
+    set timestampStart(value: number) {
+        this._timestampStart = value;
+    }
+
+    get timestampEnd(): number {
+        return this._timestampEnd;
+    }
+
+    set timestampEnd(value: number) {
+        this._timestampEnd = value;
+    }
+
+    public toJson(): any {
+        return {
+            id: this.id,
+            locationId: this.locationId,
+            timestampStart: this.timestampStart,
+            timeStampEnd: this.timestampEnd
+        }
+    }
+
+    public static fromJson(json: any): Session {
+        return new Session(
+            json.id,
+            json.locationId,
+            json.timestampStart,
+            json.timestampEnd
+        );
+    }
 
 }
